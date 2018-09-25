f46272655c0b0eeb477bfeda4acafe23ddcd6c39
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-09-25 17:08:04 -0400

    DFM's CI.

---
+++ .ci/build-paper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+TEXFILENAME="h0stat.tex"
+
+# Install tectonic using conda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create --yes -n paper
+source activate paper
+conda install -c conda-forge -c pkgw-forge tectonic
+
+# Build the paper using tectonic
+cd paper
+tectonic $TEXFILENAME --print
+
+# Force push the paper to GitHub
+cd $TRAVIS_BUILD_DIR
+git checkout --orphan $TRAVIS_BRANCH-pdf
+git rm -rf .
+git add -f paper/ms.pdf
+git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
+git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
